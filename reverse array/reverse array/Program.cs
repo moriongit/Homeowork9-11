f5c857b9cd20c8e7783097d4c0e4c5c569eb3960
moriongit <132233572+moriongit@users.noreply.github.com>
2023-10-11 01:39:53 +0400

    Add files via upload

---
+++ reverse array/reverse array/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] array = { 1, 7, 16, 5, 9 };
+
+int element = 0;         
+int reverse = array.Length-1;
+int holder = 0;
+while (element<reverse)
+{
+    //swap
+    holder = array[element];
+    array[element] = array[reverse];
+    array[reverse] = holder;
+    element++;
+    reverse--;
+}
+int[] reverseArray = array;
+
+for (int k = 0; k < reverseArray.Length; k++)
+{
+    Console.WriteLine(reverseArray[k]);
+}
